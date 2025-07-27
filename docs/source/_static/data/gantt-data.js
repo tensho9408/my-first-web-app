6c39f0c0a5fd2184d661ec20b16a63abeac30625
tensho <75622122+tensho9408@users.noreply.github.com>
2025-07-28 06:53:10 +0900

    fixed

--- docs/source/_static/data/gantt-data.js
+++ docs/source/_static/data/gantt-data.js
@@ -1,26 +1,31 @@
 // Auto-generated Mermaid Gantt chart
 window.ganttData = {
-week: `
-  gantt
-    title 学習ロードマップ（週表示）
-    dateFormat  YYYY-MM-DD
-    axisFormat  %m/%d
-    tickInterval 7day
+  "week": `
+    gantt
+      title 学習ロードマップ（週表示）
+      dateFormat  YYYY-MM-DD
+      axisFormat  %m/%d
+      tickInterval 7day
 
-    section フロントエンド
-    HTML/CSS  :a1, 2025-07-06, 14d
-    JavaScript  :a2, 2025-07-27, 2025-08-17
+      section フロントエンド
+      HTML/CSS  :a1, 2025-07-06, 14d
+      JavaScript  :a2, 2025-07-27, 2025-08-17
+      Github  :a3, 2025-07-20, 2025-07-22
 
   `,
-month: `
-  gantt
-    title 学習ロードマップ（月表示）
-    dateFormat  YYYY-MM-DD
-    axisFormat  %Y-%m
-    tickInterval 1month
+  "month": `
+    gantt
+      title 学習ロードマップ（月表示）
+      dateFormat  YYYY-MM-DD
+      axisFormat  %Y-%m
+      tickInterval 1month
 
-    section バックエンド
-    Python/Django  :b1, 2025-08-03, 2025-09-07
+      section フロントエンド
+      Next  :a3, 2025-08-20, 2025-09-22
+      Python/Django  :b2, 2025-08-03, 2025-09-21
+
+      section バックエンド
+      Python/Django  :b1, 2025-08-03, 2025-09-07
 
   `
 };
